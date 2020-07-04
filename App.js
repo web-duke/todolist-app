026d5d983e19701787436e3c8f5a58cf2ad7956f
web-duke <giovanni.mrqs@gmail.com>
2020-07-04 14:04:35 -0400

    redux init

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import { StyleSheet, SafeAreaView, Platform, View } from "react-native";
 
 import Header from "./src/components/_Shared/Header";
 import TasksContainer from "./src/components/TasksEpic/TasksContainer";
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <TasksContainer />
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <TasksContainer />
+      </SafeAreaView>
+    </Provider>
   );
 }
 
@@ -19,6 +23,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     paddingTop: Platform.OS === "android" ? 25 : 0,
     paddingLeft: 20,
-    paddingRight: 20
-  }
+    paddingRight: 20,
+  },
 });
